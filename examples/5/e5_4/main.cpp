0daf6eabf67950936b8b8d6bbbbb24f2924745ed
Oleksiy Grechnyev <shrike4625@gmail.com>
2020-02-18 14:59:54 +0200

    optional in e5_4

--- examples/5/e5_4/main.cpp
+++ examples/5/e5_4/main.cpp
@@ -1,13 +1,17 @@
 // Example 5_4 : pair, optional, any
 // Created by Oleksiy Grechnyev 2017
 
-#include <utility>
 #include <string>
 #include <iostream>
+
+#include <utility>
+#include <optional>
+#include <any>
 //=========================
 int main() {
     using namespace std;
-    
+
+    // pair
     {
         cout << "\n\nstd::pair : \n\n";
         
@@ -27,6 +31,31 @@ int main() {
         cout << p5.first << " : " << p5.second << endl;
     }
 
+    // optional
+    {
+        cout << "\n\nstd::optional\n\n" << endl;
+        
+        optional<string> o1; // Empty optional
+        optional<string> o2{"Arboga 10.2%"}; // Optional with a string
+        
+        cout << "o1.has_value() = " << o1.has_value()  << endl;
+        if (o1) {   // Check that o1 is not empty
+            cout << "o1.value() = " << o1.value() << endl;  // Throws std::bad_optional_access for empty optional
+        }
+        cout << "o1.value_or(\"Default string\") = " << o1.value_or("Default string") << endl;   // Replace by default if empty
+        
+        cout << "\no2.has_value() = " << o2.has_value()  << endl;
+        if (o2) {   // Check that o2 is not empty
+            cout << "o2.value() = " << o2.value() << endl;  // Throws std::bad_optional_access for empty optional
+        }
+        cout << "o2.value_or(\"Default string\") = " << o2.value_or("Default string") << endl;   // Replace by default if empty
+        
+    }
+    
+    // any
+    {
+        
+    }
 
     return 0;
 }
