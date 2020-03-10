023677a33a5729e565f54310e9b9fd8df30b5133
Oleksiy Grechnyev <shrike4625@gmail.com>
2020-03-10 16:25:34 +0200

    Finished example 8.3

--- examples/8/e8_3/main.cpp
+++ examples/8/e8_3/main.cpp
@@ -10,10 +10,26 @@ int main(){
     using namespace std;
         
     {
+        // sum
         vector<double> v{1., 2., 4., 7., 10.};
-        // Sum all the elements
+        
+        // Sum all elements
         cout << "sum = " << accumulate(cbegin(v), cend(v), 0.) << endl;
+        
+        // Sum all elements starting with 1. (adds 1).
+        cout << "sum (starting 1) = " << accumulate(cbegin(v), cend(v), 1.) << endl;
+        
+        // Product of all elements, with lambda
+        auto bop = [](double x, double y)->double{
+            return x*y;
+        };
+        cout << "prod1 = " << accumulate(cbegin(v), cend(v), 1., bop) << endl;
+        
+        // Product of all elements, with standard function multiplies
+        cout << "prod2 = " << accumulate(cbegin(v), cend(v), 1., multiplies<int>()) << endl;
     }
+    
+    
 
     return 0;
 }
