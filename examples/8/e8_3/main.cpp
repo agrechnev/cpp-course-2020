2531eba52c1ebdc9c082957eee7ee1df31855d39
Oleksiy Grechnyev <shrike4625@gmail.com>
2020-03-12 12:27:25 +0200

    Finished ex 8.3

--- examples/8/e8_3/main.cpp
+++ examples/8/e8_3/main.cpp
@@ -11,6 +11,7 @@ int main(){
         
     {
         // sum
+        cout << "\naccumulate : " << endl;
         vector<double> v{1., 2., 4., 7., 10.};
         
         // Sum all elements
@@ -27,6 +28,25 @@ int main(){
         
         // Product of all elements, with standard function multiplies
         cout << "prod2 = " << accumulate(cbegin(v), cend(v), 1., multiplies<int>()) << endl;
+    }
+    
+    {
+        // iota
+        cout << "\niota : " << endl;
+        vector<double> v(10);
+        // FIll array with 2 .. 11
+        iota(v.begin(), v.end(), 2.);  
+        for (double d : v )
+            cout << d << " ";
+        cout << endl;
+    }
+    
+    {
+        //Scalar product of v1, v2
+        vector<double> v1{0.5, -2, 1};
+        vector<double> v2{1, 1.5, 1};
+        cout << "\ninner_product :" << endl;
+        cout << "v1*v2 = " << inner_product(cbegin(v1), cend(v1), cbegin(v2), 0.) << endl;
     }
     
     
